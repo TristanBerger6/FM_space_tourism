14dabe77d8ffd8f9e525cdf609039e8941e6ba73
Tristan Berger <tristan.berger6@gmail.com>
2022-01-18 12:30:05 +0100

    path img js

--- nav.js
+++ nav.js
@@ -9,12 +9,12 @@ eltMobNavToggle.addEventListener("click",()=>{
     if ( ariaExpended == 'false'){
         eltNavBar.style.transform = 'translateX(0%)';
         eltMobNavIcon.setAttribute("aria-expanded","true");
-        eltMobNavToggle.style.backgroundImage = 'url("../../assets/shared/icon-close.svg")';
+        eltMobNavToggle.style.backgroundImage = 'url("./assets/shared/icon-close.svg")';
     }
     else if ( ariaExpended == 'true'){
         eltNavBar.style.transform = 'translateX(100%)';
         eltMobNavIcon.setAttribute("aria-expanded","false");
-        eltMobNavToggle.style.backgroundImage = 'url("../../assets/shared/icon-hamburger.svg")';
+        eltMobNavToggle.style.backgroundImage = 'url("./assets/shared/icon-hamburger.svg")';
     }
 })
 
